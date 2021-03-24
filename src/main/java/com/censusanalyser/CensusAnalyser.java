fdad0c7ece83eac233ac0b71bd9ff2ac370ebaa9
kunalkale98 <kunal,k2898@gmail.com>
2021-03-25 01:45:33 +0530

    Added: Custom Exception to catch Wrong File Type

--- src/main/java/com/censusanalyser/CensusAnalyser.java
+++ src/main/java/com/censusanalyser/CensusAnalyser.java
@@ -6,12 +6,13 @@ import com.opencsv.bean.CsvToBeanBuilder;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.stream.StreamSupport;
 
 public class CensusAnalyser {
-    public int loadCensusData(String csvFilePath) throws CensusAnalyserException {
+    public int loadCensusData(String csvFilePath) throws CensusAnalyserException{
         try {
             Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
             CsvToBeanBuilder<CSVStateCensus> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
@@ -22,9 +23,10 @@ public class CensusAnalyser {
             Iterable<CSVStateCensus> censusCSVIterable = () -> indiaCensusCSVIterator;
             int numOfEntries = (int) StreamSupport.stream(censusCSVIterable.spliterator(), false).count();
             return numOfEntries;
-        } catch (IOException e) {
+        } catch (RuntimeException e){
+            throw new CensusAnalyserException(e.getMessage(),CensusAnalyserException.ExceptionType.INCORRECT_FILE_TYPE);
+        } catch (IOException e){
             throw new CensusAnalyserException(e.getMessage(),CensusAnalyserException.ExceptionType.FILE_NOT_FOUND);
         }
-
     }
 }
--- src/main/java/com/censusanalyser/CensusAnalyserException.java
+++ src/main/java/com/censusanalyser/CensusAnalyserException.java
@@ -3,7 +3,8 @@ package com.censusanalyser;
 public class CensusAnalyserException extends Exception {
 
     enum ExceptionType{
-        FILE_NOT_FOUND;
+        FILE_NOT_FOUND,
+        INCORRECT_FILE_TYPE,
     }
     ExceptionType type;
     public CensusAnalyserException(String message,ExceptionType type){
